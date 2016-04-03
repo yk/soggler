4ccc8504aefb419aadf318efcd24e1abc9060f7c
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-04-03 23:14:53 +0200

    first test

---
+++ src/main/java/com/wusel/soggler/Team.java
@@ -0,0 +1,21 @@
+package com.wusel.soggler;
+
+/**
+ * Created by yk on 03/04/16.
+ */
+public class Team {
+
+    public Team(String name) {
+        //TODO
+    }
+
+    public String getName() {
+        //TODO
+        return null;
+    }
+
+    public void setName(String name) {
+        //TODO
+    }
+
+}
