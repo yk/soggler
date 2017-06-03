ce47e31037eb8bd6246357587011d0db8131b50c
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-06-03 12:55:47 +0200

    stuff

--- server/server/methods.ts
+++ server/server/methods.ts
@@ -9,13 +9,26 @@ const nonEmptyString = Match.Where((str) => {
 });
 
 Meteor.methods({
-    createTournament(name: string): void {
+    createTournament(tournament: any): void {
+        let name = tournament.name;
         check(name, nonEmptyString);
+        let teams = [];
+        for (let t=0; t<tournament.teams.length; t++){
+            if(!tournament.teams[t].name)
+                continue;
+            teams.push({
+                '_id': tournament.teams[t]._id,
+                name: tournament.teams[t].name,
+            });
+        }
+        console.log(tournament);
+        console.log(teams);
         let nt : Tournament = {
             name: name,
-            teams: [],
+            teams: teams,
             games: [],
         };
-        Tournaments.collection.insert(nt);
+        let id = Tournaments.collection.insert(nt);
+        return id;
     },
 });
